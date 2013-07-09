15859a497246e15b25b3c9ed17f404604613ec67
running-codebase <info@traviscoulter.ca>
2013-07-09 00:01:59 +0000

    fixed a typo

--- web.js
+++ web.js
@@ -13,9 +13,6 @@ app.get('/', function(request, response) {
     response.send(output);
 });
 
-
-});
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
