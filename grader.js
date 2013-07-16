7b83dece4e99c22a77f3868d5eab27e189a06927
running-codebase <info@traviscoulter.ca>
2013-07-16 05:58:12 +0000

    Initial commit

---
+++ grader.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var program = require('commander');
+var cheerio = require('cheerio');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1);
+    }
+    return instr;
+};
+
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile));
+};
+
